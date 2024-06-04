fe40ad55d3f4a3d253b39de3aa5efb7a9e9b777a
Johny Driotis <johny.driotis1996@gmail.com>
2024-06-03 22:39:17 -0700

    Se agregan imagenes de explicacion de teoria, Se agrega primer endpoint GetStudentName()

---
+++ AppFullWebAPI/AppFullWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppFullWebAPI.Controllers
+{
+    
+    [ApiController] // Se le indica al controlador que pertenece a web api.
+    [Route("api/[controller]")] // Se le indica la ruta a la que debe responder al realizar las peticiones http.
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudentName() {
+            return "Nombre de estudiante #1";
+        }
+    }
+}
