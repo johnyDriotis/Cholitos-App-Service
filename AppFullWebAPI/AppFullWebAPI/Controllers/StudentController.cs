31f28fb947aeb9d889f15fa1f72b5a0d1b799806
Johny Driotis <johny.driotis1996@gmail.com>
2024-06-04 21:47:12 -0700

    Se comenta el endpoint GetStudentName(), Se agrega el endpoint GetStudents(), Se agrega carpeta Models

--- AppFullWebAPI/AppFullWebAPI/Controllers/StudentController.cs
+++ AppFullWebAPI/AppFullWebAPI/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppFullWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppFullWebAPI.Controllers
 {
@@ -7,9 +8,28 @@ namespace AppFullWebAPI.Controllers
     [Route("api/[controller]")] // Se le indica la ruta a la que debe responder al realizar las peticiones http.
     public class StudentController : ControllerBase
     {
+        //[HttpGet]
+        //public string GetStudentName() {
+        //    return "Nombre de estudiante #1";
+        //}W
+
         [HttpGet]
-        public string GetStudentName() {
-            return "Nombre de estudiante #1";
+        public IEnumerable<Student> GetStudents() {
+            return new List<Student>
+            {
+                new Student{ 
+                    Id = 1,
+                    StudentName = "Nombre de estudiante #1",
+                    Address = "Direccion estudiante #1",
+                    Email = "studiante1@gmail.com"
+                },
+                new Student{
+                    Id = 2,
+                    StudentName = "Nombre de estudiante #2",
+                    Address = "Direccion estudiante #2",
+                    Email = "studiante2@gmail.com"
+                }
+            };
         }
     }
 }
