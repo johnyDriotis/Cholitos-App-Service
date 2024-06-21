60bf4e2567014b185b0bdf089cbe6a70ea2cd5ed
Johny Driotis <johny.driotis1996@gmail.com>
2024-06-20 21:55:01 -0600

    Aprendiendo a utilizar el routing, utilizando los route constraints

--- AppFullWebAPI/AppFullWebAPI/Controllers/StudentController.cs
+++ AppFullWebAPI/AppFullWebAPI/Controllers/StudentController.cs
@@ -3,25 +3,52 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AppFullWebAPI.Controllers
 {
-    
+
     [ApiController] // Se le indica al controlador que pertenece a web api.
     [Route("api/[controller]")] // Se le indica la ruta a la que debe responder al realizar las peticiones http.
     public class StudentController : ControllerBase
     {
-        //[HttpGet]
-        //public string GetStudentName() {
-        //    return "Nombre de estudiante #1";
-        //}
-
         [HttpGet]
+        [Route(template: "All", Name = "GetAllStudents" )]
         public IEnumerable<Student> GetStudents() {
             return CollegeRepository.Students;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet]
+        [Route(template: "{id:int}", Name = "GetStudentById")]
         public Student GetStudentById(int id)
         {
-            return CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+            var student = CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+
+            if (student != null) {
+                return student;
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route(template: "{name:alpha}", Name = "GetStudentByName")]
+        public Student GetStudentByName(string name)
+        {
+            var student = CollegeRepository.Students.Where(x => x.StudentName == name).FirstOrDefault();
+
+            if (student != null)
+            {
+                return student;
+            }
+            return null;
+        }
+
+        [HttpDelete]
+        [Route(template: "{id:int}", Name = "DeleteStudent")]
+        public bool DeleteStudent(int id) { 
+            var student = CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+
+            if (student != null) {
+                CollegeRepository.Students.Remove(student);
+                return true;
+            }
+            return false;
         }
     }
 }
