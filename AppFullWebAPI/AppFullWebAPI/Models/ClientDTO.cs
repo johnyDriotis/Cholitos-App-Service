d93d8ba339d14a8b7bdc4a3186a2c726e879852d
johnyDriotis <johny.driotis1996@gmail.com>
2024-08-03 15:29:44 -0600

    Trabajando con validaciones de campos por medio de atributos(dataannotations)

--- AppFullWebAPI/AppFullWebAPI/Models/ClientDTO.cs
+++ AppFullWebAPI/AppFullWebAPI/Models/ClientDTO.cs
@@ -1,20 +1,34 @@
-﻿namespace CholitosGymWebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CholitosGymWebApi.Models
 {
     public class ClientDTO
     {
         #region Propiedades Not Null
         public int CodigoCliente { get; set; }
+
+        [Range(minimum: 1, maximum: 100, ConvertValueInInvariantCulture = false, ErrorMessage = "La Edad debe estar entre 1 y 100 anios")]
         public int Edad { get; set; }
         #endregion
 
         #region Propiedades Null
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Primer nombre es requerido")]
         public string? PrimerNombre { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Segundo nombre es requerido")]
         public string? SegundoNombre { get; set; }
+
         public string? TercerNombre { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Primer apellido es requerido")]
         public string? PrimerApellido { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Segundo apellido es requerido")]
         public string? SegundoApellido { get; set; }
         public string? Genero { get; set; }
         public string? ApellidoCasada { get; set; }
+
+        [EmailAddress(ErrorMessage = "El correo electronico es invaalido")]
         public string? CorreoElectronico { get; set; }
         #endregion
     }
