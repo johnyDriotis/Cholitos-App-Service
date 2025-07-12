9ed6153c3d7fefa44b5c0fa579d7e13664c96737
johnyDriotis <johny.driotis1996@gmail.com>
2025-07-12 08:00:13 -0600

    Se elimina proyecto worker para crearlo de manera correcta

--- CholitosGymService/CholitosGymService.FingerPrintBackgroundService/CholitosGymService.FingerPrintBackgroundTask/Program.cs
+++
@@ -1,7 +0,0 @@
-using CholitosGymService.FingerPrintBackgroundTask;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
